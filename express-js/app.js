ac6b4db226e86139bb4be60937344db44df34ecb
kelompokptik <cyberlast173@gmail.com>
2024-09-12 20:06:05 +0700

    view Angine

--- express-js/app.js
+++ express-js/app.js
@@ -1,22 +1,47 @@
 const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-  //   res.json({
-  //     nama: "Ubay Dillah",
-  //     noHp: 12121,
-  //   });
+// menggunakan ejs
+app.set("view engine", "ejs");
+app.use(expressLayouts);
 
-  res.sendFile("./index.html", { root: __dirname });
+app.get("/", (req, res) => {
+  //   res.sendFile("./index.html", { root: __dirname });
+  const mahasiswa = [
+    {
+      nama: "Ubay Dillah",
+      email: "ubaydillah@gmail.com",
+    },
+    {
+      nama: "Adrien Mozatrya",
+      email: "adriengaming123@gmail.com",
+    },
+    {
+      nama: "Ikhsan gaming",
+      email: "nurikhsan@gmail.com",
+    },
+  ];
+
+  res.render("index", {
+    layout: "layouts/main-layout",
+    nama: "Ubay Dillah",
+    title: "Home",
+    mahasiswa,
+  });
 });
 
 app.get("/about", (req, res) => {
-  res.send("About");
+  //   res.send("About");
+  res.render("about", {
+    layout: "layouts/main-layout",
+    title: "about",
+  });
 });
 
 app.get("/contact", (req, res) => {
-  res.send("contact");
+  res.render("contact", { layout: "layouts/main-layout", title: "contact" });
 });
 
 app.get("/product/:id", (req, res) => {
